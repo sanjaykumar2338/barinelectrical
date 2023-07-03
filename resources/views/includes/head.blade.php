1add63ed6900ebbd690ac7a337840806bf35e505
Sanjay Kumar <sk963070@gmail.com>
2023-07-03 16:12:23 +0530

    favicon added

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -17,5 +17,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="icon" type="image/x-icon" href="{{url('/asset/frontend/images/barin-mobile-logo-1.png')}}">
+<link rel="icon" type="image/x-icon" href="{{url('/asset/frontend/images/favicon.jpg')}}">
 
